22191afd934b7c2f91154d9a260973f87b282bda
Paulo <pauljr235@gmail.com>
2024-05-21 19:52:47 +0100

    primeiro commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('form-projeto');
+const nomes = [];
+const numeros = [];
+
+
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionarLinha();
+})
+
+function adicionarLinha(){
+    const inputNomeDoContato = document.getElementById('nome-contato');
+    const inputNumeroDoContato = document.getElementById('numero-contato');
+
+    if(nomes.includes(inputNomeDoContato.value)){
+        alert(`O nome ${inputNomeDoContato.value} já foi inserido na agenda!`);
+    } else{
+        nomes.push(inputNomeDoContato.value);
+        numeros.push(inputNumeroDoContato.value);
+    
+        let linha = '<tr>';
+        linha += `<td> ${inputNomeDoContato.value}</td>`;
+        linha += `<td> ${inputNumeroDoContato.value}</td>`;
+        linha += '</tr>';
+    
+        linhas += linha;    
+    
+        const corpoTabela = document.querySelector('tbody'); /* atualiza a tabela com os inputs do usuário*/
+        corpoTabela.innerHTML = linhas; /* atualiza a tabela com os inputs do usuario*/
+        
+        inputNomeDoContato = '';
+        inputNumeroDoContato = '';
+    }
+}
+
